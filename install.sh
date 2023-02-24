5bb0136eb4dc35cfb973d3c75f607e24c59c2c76
EasyWay <walterofice@gmail.com>
2023-02-25 00:39:14 +0200

    button install theme

--- install.sh
+++ install.sh
@@ -7,6 +7,22 @@ fi
 
 clear
 
+installTheme(){
+    cd /var/www/pterodactyl
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    apt update
+    apt install -y nodejs
+
+    npm i -g yarn
+    yarn
+
+    cd /var/www/pterodactyl
+    yarn build:production
+    sudo php artisan optimize:clear
+
+
+}
+
 repair(){
     bash <(curl https://raw.githubusercontent.com/coreua/recovery-pterodactyl/main/repair.sh)
 }
@@ -14,7 +30,8 @@ repair(){
 echo "Copyright (c) 2023 CoreUa | admin@coreua.top"
 echo ""
 echo "[1] Repair panel (use if you have an error in the theme installation)"
-echo "[2] Exit"
+echo "[2] Install theme"
+echo "[3] Exit"
 
 read -p "Please enter a number: " choice
 if [ $choice == "1" ]
@@ -22,6 +39,10 @@ if [ $choice == "1" ]
     repair
 fi
 if [ $choice == "2" ]
+    then
+    installTheme
+fi
+if [ $choice == "3" ]
     then
     exit
 fi
